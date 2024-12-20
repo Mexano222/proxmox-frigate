e4f7ba9c98c7b5cb0ca7e87c4b7dce6a04a273f1
Mexano222 <mexano222@email.com>
2024-12-20 12:08:35 +0200

    updated links to repo

--- install/frigate-install.sh
+++ install/frigate-install.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
 msg_info "Installing Docker"
-bash -c "$(wget -qLO - https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/install/docker.sh)" || exit
+bash -c "$(wget -qLO - https://raw.githubusercontent.com/Mexano222/proxmox-frigate/main/install/docker.sh)" || exit
 msg_ok "Installed Docker"
 
 msg_info "Installing Nvidia drivers"
-bash -c "$(wget -qLO - https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/install/nvidia.sh)" || exit
+bash -c "$(wget -qLO - https://raw.githubusercontent.com/Mexano222/proxmox-frigate/main/install/nvidia.sh)" || exit
 msg_ok "Installed Nvidia drivers"
 
 msg_info "Installing Frigate container"
-bash -c "$(wget -qLO - https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/install/frigate.sh)" || exit
+bash -c "$(wget -qLO - https://raw.githubusercontent.com/Mexano222/proxmox-frigate/main/install/frigate.sh)" || exit
 msg_ok "Installed Frigate container"
