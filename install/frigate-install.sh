1bc899bc8d6740b9eef27f8daeaa73529649a8e6
Mexano222 <mexano222@email.com>
2024-12-23 08:23:29 +0200

    update frigate-install

--- install/frigate-install.sh
+++ install/frigate-install.sh
@@ -7,7 +7,8 @@ catch_errors
 setting_up_container
 network_check
 update_os
-
+msg_info
+msg_ok
 
 msg_info "Installing Docker"
 bash -c "$(wget -qLO - https://raw.githubusercontent.com/Mexano222/proxmox-frigate/main/install/docker.sh)" || exit
