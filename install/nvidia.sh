436bd7ee9ded9ec1cc52d17ebb0a617c870bafec
Mexano222 <mexano222@email.com>
2024-12-23 13:43:53 +0200

    update nvidia.sh

--- install/nvidia.sh
+++ install/nvidia.sh
@@ -4,7 +4,7 @@ wget https://us.download.nvidia.com/XFree86/Linux-x86_64/565.57.01/NVIDIA-Linux-
 chmod +x NVIDIA-Linux-x86_64-565.57.01.run
 ./NVIDIA-Linux-x86_64-565.57.01.run --no-kernel-module
 
-msg_info "Cleaning up"
+echo "Cleaning up"
 $STD apt-get -y autoremove
 $STD apt-get -y autoclean
-msg_ok "Cleaned"
+echo "Cleaned"
